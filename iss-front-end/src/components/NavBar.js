5e900d76fdcb730f17bc31d9c022593b5c12d06e
dislersd <dislersd@gmail.com>
2019-02-07 15:23:12 -0800

    added styling to main inventory list

--- iss-front-end/src/components/NavBar.js
+++ iss-front-end/src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Authenticate from './Authenticate'
 import { Route } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
+import './NavBar.css';
 
 const NavBar = () =>{
   return(
