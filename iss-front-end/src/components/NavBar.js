0c4f35db90d9967a9c9504f738a2de0b693d9eea
Josh Z <jzieger10@gmail.com>
2019-02-07 18:22:11 -0500

    added content to NavBar component

--- iss-front-end/src/components/NavBar.js
+++ iss-front-end/src/components/NavBar.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Authenticate from './Authenticate'
-import { Route } from 'react-router-dom'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import Authenticate from "./Authenticate";
+import { Route } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-const NavBar = () =>{
-  return(
-    <div>
-
-
-    </div>
-  )
-}
-
+const NavBar = () => {
+	return (
+		<nav>
+			<NavLink to="/display">DisplayInventory</NavLink>
+			<NavLink to="/inventory" />
+			<NavLink to="/locations">Locations</NavLink>
+			<NavLink to="/volunteerRegister">Register Volunteer</NavLink>
+			<NavLink to="/volunteerLogin">Login Volunteer</NavLink>
+		</nav>
+	);
+};
 
 export default NavBar;
