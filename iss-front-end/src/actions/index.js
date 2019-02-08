49ba0e6d99a6b9b7ab632119c41d040471e227b6
Josh Z <jzieger10@gmail.com>
2019-02-08 07:20:30 -0500

    minor nav styling tweak

--- iss-front-end/src/actions/index.js
+++ iss-front-end/src/actions/index.js
@@ -1,7 +1,13 @@
-import * as inventoryActions from './inventoryActions'
-import * as locationActions from './locationActions'
-import * as userAcctActions from './userAcctActions'
-import * as userActions from './userActions'
-import * as volunteersActions from './volunteersActions'
+import * as inventoryActions from "./inventoryActions";
+import * as locationActions from "./locationActions";
+import * as userAcctActions from "./userAcctActions";
+import * as userActions from "./userActions";
+import * as volunteersActions from "./volunteersActions";
 
-export { inventoryActions, locationActions, userAcctActions, userActions, volunteersActions }
+export {
+	inventoryActions,
+	locationActions,
+	userAcctActions,
+	userActions,
+	volunteersActions,
+};
